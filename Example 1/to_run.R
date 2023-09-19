0395dbaa30eb7b0ff657f6162dc07017b48ce319
Davide <davide.messina@ars.toscana.it>
2023-09-19 12:29:00 +0200

    updated example 1

--- Example 1/to_run.R
+++ Example 1/to_run.R
@@ -13,18 +13,20 @@ TD_variable[, value_of_in_study_period := NULL]
 combined_df <- TD_variable[D_EVENT, on = "person_id", date_of_event := i.date_of_event]
 # rm(TD_variable, D_EVENT)
 
-# Left join of spells and events
-df_between <- combined_df[date_of_event > start_date & date_of_event <= end_date]
+# Split the data.table in two
+combined_df <- split(combined_df, combined_df[, fifelse(date_of_event > start_date & date_of_event <= end_date, 1, 0, 0)])
 
-df_trivial <- combined_df[is.na(date_of_event) | date_of_event <= start_date | date_of_event > end_date]
-df_trivial[, date_of_event := data.table::fifelse(!is.na(date_of_event) & date_of_event <= start_date, 1, 0)]
-data.table::setnames(df_trivial, "date_of_event", "value_of_variable")
+# First data.table is trivial
+combined_df[[1]][, date_of_event := data.table::fifelse(!is.na(date_of_event) & date_of_event <= start_date, 1, 0)]
+data.table::setnames(combined_df[[1]], "date_of_event", "value_of_variable")
 # rm(combined_df)
 
-df_between_before <- copy(df_between)[, end_date := date_of_event - 1][, date_of_event := NULL][, value_of_variable := 0]
-df_between_after <- df_between[, start_date := date_of_event][, date_of_event := NULL][, value_of_variable := 1]
+# Create a copy of the second one and then censor the exit and entry
+combined_df[[3]] <- copy(combined_df[[2]])[, end_date := date_of_event - 1][, date_of_event := NULL][, value_of_variable := 0]
+combined_df[[2]][, start_date := date_of_event][, date_of_event := NULL][, value_of_variable := 1]
 # rm(df_between)
 
-final_df <- data.table::rbindlist(list(df_trivial, df_between_before, df_between_after))
+# Combine and order the data.tables
+final_df <- data.table::rbindlist(combined_df)
 # rm(df_trivial, df_between_before, df_between_after)
 data.table::setorder(final_df, "person_id", "start_date")
