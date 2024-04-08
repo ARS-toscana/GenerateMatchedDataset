7671825e56dbc13c7cf132014160ee38276ff27f
Davide <davide.messina@ars.toscana.it>
2024-04-08 18:16:24 +0200

    created example 2

---
+++ Example 2/to_run.R
@@ -0,0 +1,15 @@
+source(file.path("R", "GenerateMatchedDataset.R"))
+
+GenerateMatchedDataset(exposed = data.table::fread("test_with_simulated_data/exposed_1000.csv"),
+                       candidate_matches = data.table::fread("test_with_simulated_data/candidate_matches_1000.csv"),
+                       unit_of_observation = c("person_id"),
+                       time_variable_in_exposed = c("vax1_day"),
+                       time_variables_in_candidate_matches = c("start", "end"),
+                       variables_with_exact_matching = c("SES", "REGION"),
+                       # variables_with_range_matching = c("age"),
+                       range_of_variables_with_range_matching = list(c(-1, 1)),
+                       sample_size_per_exposed = 1,
+                       number_of_bootstrapping_samples = 10,
+                       threshold = 200000,
+                       temporary_folder = c("Example 2/temp"))
+
