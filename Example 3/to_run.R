6254069b75f339010539077a41f5ba18b050eaa0
Davide Messina <davide.messina@ars.toscana.it>
2024-04-14 18:06:01 +0200

    Added performance comparison to example 3

--- Example 3/to_run.R
+++ Example 3/to_run.R
@@ -2,40 +2,13 @@ rm(list=ls(all.names=TRUE))
 source(file.path("R", "GenerateMatchedDataset.R"))
 source(file.path("R", "GenerateMatchedDatasetNaive.R"))
 source(file.path("R", "GenerateMatchedDatasetHT.R"))
-unlink("Example 2/g_intermediate", recursive = T)
-unlink("Example 2/g_output", recursive = T)
-dir.create("Example 2/g_intermediate")
-dir.create("Example 2/g_output")
+unlink("Example 3/g_intermediate", recursive = T)
+unlink("Example 3/g_output", recursive = T)
+dir.create("Example 3/g_intermediate")
+dir.create("Example 3/g_output")
 
-GenerateMatchedDataset(exposed = data.table::fread("test_with_simulated_data/exposed_1000.csv"),
-                       candidate_matches = data.table::fread("test_with_simulated_data/candidate_matches_1000.csv"),
-                       unit_of_observation = c("person_id"),
-                       time_variable_in_exposed = c("vax1_day"),
-                       time_variables_in_candidate_matches = c("start", "end"),
-                       variables_with_exact_matching = c("SES", "REGION"),
-                       variables_with_range_matching = c("age"),
-                       range_of_variables_with_range_matching = list(c(-1, 1)),
-                       sample_size_per_exposed = 1,
-                       number_of_bootstrapping_samples = 10,
-                       threshold = 2000000000,
-                       temporary_folder = c("Example 2/g_intermediate"),
-                       output_matching = c("Example 2/g_output"))
-
-GenerateMatchedDatasetNaive(exposed = data.table::fread("test_with_simulated_data/exposed_1000.csv"),
-                            candidate_matches = data.table::fread("test_with_simulated_data/candidate_matches_1000.csv"),
-                            unit_of_observation = c("person_id"),
-                            time_variable_in_exposed = c("vax1_day"),
-                            time_variables_in_candidate_matches = c("start", "end"),
-                            variables_with_exact_matching = c("SES", "REGION"),
-                            variables_with_range_matching = c("age"),
-                            range_of_variables_with_range_matching = list(c(-1, 1)),
-                            sample_size_per_exposed = 1,
-                            number_of_bootstrapping_samples = 10,
-                            threshold = 2000000000,
-                            temporary_folder = c("Example 2/g_intermediate"),
-                            output_matching = c("Example 2/g_output"))
-
-GenerateMatchedDatasetHT(exposed = data.table::fread("test_with_simulated_data/exposed_1000.csv"),
+bench::mark({
+  GenerateMatchedDataset(exposed = data.table::fread("test_with_simulated_data/exposed_1000.csv"),
                          candidate_matches = data.table::fread("test_with_simulated_data/candidate_matches_1000.csv"),
                          unit_of_observation = c("person_id"),
                          time_variable_in_exposed = c("vax1_day"),
@@ -46,12 +19,41 @@ GenerateMatchedDatasetHT(exposed = data.table::fread("test_with_simulated_data/e
                          sample_size_per_exposed = 1,
                          number_of_bootstrapping_samples = 10,
                          threshold = 2000000000,
-                         temporary_folder = c("Example 2/g_intermediate"),
-                         output_matching = c("Example 2/g_output"))
+                         temporary_folder = c("Example 3/g_intermediate"),
+                         output_matching = c("Example 3/g_output"))
+}, {
+  GenerateMatchedDatasetNaive(exposed = data.table::fread("test_with_simulated_data/exposed_1000.csv"),
+                              candidate_matches = data.table::fread("test_with_simulated_data/candidate_matches_1000.csv"),
+                              unit_of_observation = c("person_id"),
+                              time_variable_in_exposed = c("vax1_day"),
+                              time_variables_in_candidate_matches = c("start", "end"),
+                              variables_with_exact_matching = c("SES", "REGION"),
+                              variables_with_range_matching = c("age"),
+                              range_of_variables_with_range_matching = list(c(-1, 1)),
+                              sample_size_per_exposed = 1,
+                              number_of_bootstrapping_samples = 10,
+                              threshold = 2000000000,
+                              temporary_folder = c("Example 3/g_intermediate"),
+                              output_matching = c("Example 3/g_output"))
+}, {
+  GenerateMatchedDatasetHT(exposed = data.table::fread("test_with_simulated_data/exposed_1000.csv"),
+                           candidate_matches = data.table::fread("test_with_simulated_data/candidate_matches_1000.csv"),
+                           unit_of_observation = c("person_id"),
+                           time_variable_in_exposed = c("vax1_day"),
+                           time_variables_in_candidate_matches = c("start", "end"),
+                           variables_with_exact_matching = c("SES", "REGION"),
+                           variables_with_range_matching = c("age"),
+                           range_of_variables_with_range_matching = list(c(-1, 1)),
+                           sample_size_per_exposed = 1,
+                           number_of_bootstrapping_samples = 10,
+                           threshold = 2000000000,
+                           temporary_folder = c("Example 3/g_intermediate"),
+                           output_matching = c("Example 3/g_output"))
+}, min_iterations = 10)
 
-bootstrap_sample_original <- qs::qread(file.path("Example 2/g_output", paste0("bootstrap_", 1)))
-bootstrap_sample_naive <- qs::qread(file.path("Example 2/g_output", paste0("bootstrap_naive_", 1)))
-bootstrap_sample_HT <- qs::qread(file.path("Example 2/g_output", paste0("bootstrap_HT_", 1)))
+bootstrap_sample_original <- qs::qread(file.path("Example 3/g_output", paste0("bootstrap_", 1)))
+bootstrap_sample_naive <- qs::qread(file.path("Example 3/g_output", paste0("bootstrap_naive_", 1)))
+bootstrap_sample_HT <- qs::qread(file.path("Example 3/g_output", paste0("bootstrap_HT_", 1)))
 data.table::setorder(bootstrap_sample_original, person_id, i.person_id)
 data.table::setorder(bootstrap_sample_naive, person_id, i.person_id)
 data.table::setorder(bootstrap_sample_HT, person_id, i.person_id)
