606f1653268dcb0573904e5f09699d9ac4c4e76b
Davide <davide.messina@ars.toscana.it>
2024-05-06 17:01:02 +0200

    cleaned launching of steps added g_parameters in .gitignore

--- Experiment/p_parameters/1_general_parameters.R
+++ Experiment/p_parameters/1_general_parameters.R
@@ -6,3 +6,21 @@ invisible(lapply(list.of.packages, require, character.only = T))
 
 # Clean environment
 rm(new.packages, list.of.packages)
+
+# Function to launch step
+launch_step <- function(x, print.eval = F) {
+  
+  if ("folder" %in% ls(envir = .GlobalEnv)) {
+    folder <- get("folder", envir = .GlobalEnv)
+  } else {
+    stop("Folder of the generated parameters is not called 'dirpargen'")
+  }
+  
+  exec_time <- system.time(source(file.path(folder, "p_steps", x), print.eval = print.eval))
+  print(exec_time)
+  
+  rm(list=ls(envir = .GlobalEnv), envir = .GlobalEnv)
+  load(file.path(folder, "g_parameters", "parameters.RData"), envir = .GlobalEnv)
+  rm(list=ls(), inherits = T)
+  
+}
--- Experiment/to_run.R
+++ Experiment/to_run.R
@@ -15,21 +15,25 @@ unlink(file.path(folder, "g_intermediate"), recursive = T)
 unlink(file.path(folder, "g_output"), recursive = T)
 unlink(file.path(folder, "g_results"), recursive = T)
 unlink(file.path(folder, "g_datasets"), recursive = T)
+unlink(file.path(folder, "g_parameters"), recursive = T)
+
 # Create again folders
 dir.create(file.path(folder, "g_intermediate"))
 dir.create(file.path(folder, "g_output"))
 dir.create(file.path(folder, "g_results"))
 dir.create(file.path(folder, "g_datasets"))
+dir.create(file.path(folder, "g_parameters"))
 
 # Set the experiment parameters
 source(file.path(folder, "p_parameters", "1_general_parameters.R"))
 source(file.path(folder, "p_parameters", "2_parameters_datasets.R"))
 source(file.path(folder, "p_parameters", "3_generate_complete_experiment_combinations.R"))
+source(file.path(folder, "p_parameters", "99_saving_all_parameters.R"))
 
 # Generate datasets
-source(file.path(folder, "p_steps", "step_1_generate_datasets.R"))
-source(file.path(folder, "p_steps", "step_2_run_simulations.R"))
-source(file.path(folder, "p_steps", "step_3_run_analysis.R"))
-source(file.path(folder, "p_steps", "step_4_tables_and_figures.R"))
+launch_step("step_1_generate_datasets.R")
+launch_step("step_2_run_simulations.R")
+launch_step("step_3_run_analysis.R")
+launch_step("step_4_tables_and_figures.R")
 
 rm(folder)
