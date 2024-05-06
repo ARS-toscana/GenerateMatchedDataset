42fb02b51aa470a289edc55fa8f315e8e9bdbb35
Davide <davide.messina@ars.toscana.it>
2024-05-06 23:07:02 +0200

    Fixed restriction of datasets

--- Experiment/p_parameters/2_parameters_datasets.R
+++ Experiment/p_parameters/2_parameters_datasets.R
@@ -18,7 +18,7 @@ df_size_exposed <- c(100L, 200L, 1000L, 2000L, 10000L, 20000L, 100000L, 200000L,
 df_size_exposed_lab <- c("1h", "2h", "1k", "2k", "10k", "20k", "100k", "200k", "1M")
 names(df_size_exposed) <- df_size_exposed_lab
 rm(df_size_exposed_lab)
-df_size_exposed[1:(length(df_size_exposed) - data.table::fcase(level_restriction == 0, 0,
+df_size_exposed <- df_size_exposed[1:(length(df_size_exposed) - data.table::fcase(level_restriction == 0, 0,
                                                                level_restriction == 1, 1,
                                                                level_restriction == 2, 3,
                                                                level_restriction == 3, 5,
@@ -29,7 +29,7 @@ cm_factor <- c(0.1, 2, 10, 100, 1000, 10000, 100000, 1000000)
 cm_factor <- as.numeric(format(cm_factor, scientific = FALSE))
 cm_factor_lab <- c("1dt","2t","10t","100t","1kt","10kt","100kt","1Mt")
 names(cm_factor) <- cm_factor_lab
-cm_factor[1:(length(cm_factor) - level_restriction)]
+cm_factor <- cm_factor[1:(length(cm_factor) - level_restriction)]
 
 #-------------------------------------
 # all combinations
@@ -49,5 +49,5 @@ for (i in 1:length(df_size_exposed)) {
   }
 }
 
-rm(df_size_exposed, cm_factor, cm_factor_lab, i)
+rm(df_size_exposed, cm_factor, cm_factor_lab, i, level_restriction)
 
