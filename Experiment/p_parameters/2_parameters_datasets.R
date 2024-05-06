36dc936051eb707ebced6fad918da563b478f4e4
Davide <davide.messina@ars.toscana.it>
2024-05-06 19:37:41 +0200

    Restrict dataset creation to only smaller ones

--- Experiment/p_parameters/2_parameters_datasets.R
+++ Experiment/p_parameters/2_parameters_datasets.R
@@ -10,17 +10,26 @@
 # cm          - factor of candidate matches
 # label_cm    - label of cm_factor_ds
 
+# Restrict datasets creation in case bigger ones are not necessary (values ranges from 0 to 4)
+level_restriction <- 3
+
 # list of all possible sizes of exposed
 df_size_exposed <- c(100L, 200L, 1000L, 2000L, 10000L, 20000L, 100000L, 200000L, 1000000L)
 df_size_exposed_lab <- c("1h", "2h", "1k", "2k", "10k", "20k", "100k", "200k", "1M")
 names(df_size_exposed) <- df_size_exposed_lab
 rm(df_size_exposed_lab)
+df_size_exposed[1:(length(df_size_exposed) - data.table::fcase(level_restriction == 0, 0,
+                                                               level_restriction == 1, 1,
+                                                               level_restriction == 2, 3,
+                                                               level_restriction == 3, 5,
+                                                               level_restriction == 4, 7))]
 
 # list of all possible factors to create candidate matches
 cm_factor <- c(0.1, 2, 10, 100, 1000, 10000, 100000, 1000000)
 cm_factor <- as.numeric(format(cm_factor, scientific = FALSE))
 cm_factor_lab <- c("1dt","2t","10t","100t","1kt","10kt","100kt","1Mt")
 names(cm_factor) <- cm_factor_lab
+cm_factor[1:(length(cm_factor) - level_restriction)]
 
 #-------------------------------------
 # all combinations
